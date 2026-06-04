e43a99005c22e566a4bfaf35da5eeaafb679917a
haziq <haziq@10.111.246.98>
2026-06-04 14:15:40 +0800

    akmal punya

---
+++ src/main/java/Employee/Employee.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Employee;
+
+/**
+ *
+ * @author haziq
+ */
+public class Employee {
+    private String name;
+    public Employee( String name){
+        this.name=name;
+    }
+}
