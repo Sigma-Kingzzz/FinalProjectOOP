14c6954b5184ea84a16977344d21577ced1303dc
User <User@10.145.51.63>
2026-06-08 22:42:00 +0800

    heh

--- src/main/java/AdminPage.java
+++ src/main/java/AdminPage.java
@@ -69,6 +69,7 @@ public class AdminPage extends Application {
         
         Scene scene = new Scene(pane, 500, 500);
         stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
