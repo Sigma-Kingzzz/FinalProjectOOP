aa780b8ab771a1cdb3e222b64d296bc092a43a5f
Ammar Asrafi <ammar.asrafi123@gmail.com>
2026-06-09 12:10:57 +0800

    Update Salary.java

--- src/main/java/Salary.java
+++ src/main/java/Salary.java
@@ -143,7 +143,7 @@ public class Salary {
 
         Scene scene = new Scene(pane, 400, 500);
         Stage stage = new Stage();
-        stage.setTitle("Salary Breakdown");
+        stage.setTitle("Pay Slip");
         stage.setScene(scene);
         stage.show();
     }
